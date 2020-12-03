2b86012fc3daa3565510637f6761b743a84a3d87
techchamp88 <74140404+techchamp88@users.noreply.github.com>
2020-12-03 11:35:40 +0900

    CSSファイルの設置と上手く行ってなかったdb:migrateの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many   :items
-  has_many   :user_items
+         has_many   :items
+         has_many   :user_items
+       
 
-  validates :nickname,         presence: true
-  validates :last_name,        presence: true
-  validates :first_name,       presence: true
-  validates :last_name_kana,   presence: true
-  validates :first_name_kana,  presence: true
-  validates :birthday,         presence: true
-
+         validates :nickname,         presence: true
+         validates :last_name,        presence: true
+         validates :first_name,       presence: true
+         validates :last_name_kana,   presence: true
+         validates :first_name_kana,  presence: true
+         validates :birthday,         presence: true
 end
--- db/migrate/20201203020802_devise_create_users.rb
+++ db/migrate/20201203020802_devise_create_users.rb
@@ -13,7 +13,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :first_name_kana,    null: false
       t.date   :birthday,           null: false
 
-
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
