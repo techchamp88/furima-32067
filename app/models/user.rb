b9487694bd96a92a3ecb68ecce1edc195958eb79
techchamp88 <74140404+techchamp88@users.noreply.github.com>
2020-12-04 12:58:36 +0900

    ユーザー管理機能　編集１

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,23 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         has_many   :items
-         has_many   :user_items
+        #  has_many   :items
+        #  has_many   :user_items
        
 
          validates :nickname,         presence: true
 
-         VALID_LAST_NAME =/\A[ぁ-んァ-ン一-龥]/.freeze
-         validates :last_name,        presence: true, format: { with: VALID_LAST_NAME}
-
-         VALID_FIRST_NAME =/\A[ぁ-んァ-ン一-龥]/.freeze
-         validates :first_name,       presence: true, format: { with: VALID_FIRST_NAME}
+         VALID_NAME =/\A[ぁ-んァ-ン一-龥]/.freeze
+         validates :last_name,        presence: true, format: { with: VALID_NAME}
+         validates :first_name,       presence: true, format: { with: VALID_NAME}
         
-         VALID_LAST_NAME_KATAKANA = /\A[ァ-ヶー－]+\z/.freeze
-         validates :last_name_kana,   presence: true, format: { with: VALID_LAST_NAME_KATAKANA}
-
-         VALID_FIRST_NAME_KATAKANA = /\A[ァ-ヶー－]+\z/.freeze
-         validates :first_name_kana,  presence: true, format: { with: VALID_FIRST_NAME_KATAKANA}
+         VALID_NAME_KATAKANA = /\A[ァ-ヶー－]+\z/.freeze
+         validates :last_name_kana,   presence: true, format: { with: VALID_NAME_KATAKANA}
+         validates :first_name_kana,  presence: true, format: { with: VALID_NAME_KATAKANA}
 
 
          validates :birthday,         presence: true
