bb5085acbacf668b72ae14006ef87a8d8b7304a8
techchamp88 <74140404+techchamp88@users.noreply.github.com>
2020-12-07 04:54:54 +0900

    バリデーション、アソシエーションなどの設定とgemのインストール

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,16 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one    :user_item
+  has_one_attached :image
+
+  validates :name,            presence: true
+  validates :explanation,     presence: true
+  validates :category_id,     presence: true
+  validates :status_id,       presence: true
+  validates :delivery_fee_id, presence: true
+  validates :shipper_id,      presence: true
+  validates :delivery_day_id, presence: true
+  validates :price,           presence: true
+  validates :name,            presence: true
+  validates :image,           presence: true
 end
