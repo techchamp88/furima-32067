0638b997ba9e28a4406d4bc1ae26f8d4f010d5ac
techchamp88 <74140404+techchamp88@users.noreply.github.com>
2020-12-07 09:21:15 +0900

    アクティブハッシュの導入とアソシエーションなどの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,16 @@ class Item < ApplicationRecord
   has_one    :user_item
   has_one_attached :image
 
+
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_fee
+  belongs_to :shipper
+  belongs_to :delivery_day
+
+
+
+
   validates :name,            presence: true
   validates :explanation,     presence: true
   validates :category_id,     presence: true
@@ -13,4 +23,11 @@ class Item < ApplicationRecord
   validates :price,           presence: true
   validates :name,            presence: true
   validates :image,           presence: true
+
+
+  validates :category_id,     numericality: { other_than: 1 } 
+  validates :status_id,       numericality: { other_than: 1 } 
+  validates :delivery_fee_id, numericality: { other_than: 1 } 
+  validates :shipper_id,      numericality: { other_than: 1 } 
+  validates :delivery_day_id, numericality: { other_than: 1 } 
 end
