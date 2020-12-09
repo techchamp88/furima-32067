3351c22b743fdc076df09fcc15d9856527b4066f
techchamp88 <74140404+techchamp88@users.noreply.github.com>
2020-12-09 18:22:34 +0900

    出品者以外が削除できないように記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,6 +38,7 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
+    redirect_to root_path unless current_user.id == item.user_id
     item.destroy
     redirect_to root_path
   end
