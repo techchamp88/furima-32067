1c68249075be51e07c17b7425f9e8137103ae043
techchamp88 <74140404+techchamp88@users.noreply.github.com>
2020-12-14 20:05:53 +0900

    addressesテーブルと中間テーブルの作成

---
+++ db/migrate/20201214102902_create_addresses.rb
@@ -0,0 +1,14 @@
+class CreateAddresses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :addresses do |t|
+      t.integer   :prefecture_id,        null: false
+      t.string   :post_code,   null: false
+      t.string   :city,   null: false
+      t.string   :address,   null: false
+      t.string   :building,   
+      t.string   :tel_num,   null: false
+      t.string   :filename,   null: false
+      t.timestamps
+    end
+  end
+end
