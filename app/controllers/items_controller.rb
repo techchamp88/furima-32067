4d7c6e322649c109f6f97d0d7656b2e2468704f5
techchamp88 <74140404+techchamp88@users.noreply.github.com>
2020-12-18 11:10:49 +0900

    編集画面の編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,7 +25,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to root_path if current_user.id != @item.user_id || @item.user_item.present
+    redirect_to root_path if current_user.id != @item.user_id || @item.user_item.present?
   end
 
   def update
